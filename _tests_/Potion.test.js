91bce16765fa06e1663eab2d9b0c704b8f713578
Jacob Banks <kobybanks@gmail.com>
2021-07-21 11:59:06 -0400

    done Player!!!

--- _tests_/Potion.test.js
+++ _tests_/Potion.test.js
@@ -1,4 +1,5 @@
 const Potion = require("../lib/Potion.js");
+const Player = require("../lib/Player.js");
 test("creates a health potion object", () => {
   const potion = new Potion("health");
 
@@ -11,4 +12,13 @@ test("creates a random potion object", () => {
   expect(potion.name).toEqual(expect.any(String));
   expect(potion.name.length).toBeGreaterThan(0);
   expect(potion.value).toEqual(expect.any(Number));
+});
+test("uses a potion from inventory", () => {
+  const player = new Player("Dave");
+  player.inventory = [new Potion(), new Potion(), new Potion()];
+  const oldCount = player.inventory.length;
+
+  player.usePotion(1);
+
+  expect(player.inventory.length).toBeLessThan(oldCount);
 });
